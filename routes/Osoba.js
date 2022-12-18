78c2621cb8920fd17af2a3f6483eb6d8f1f8b2a4
Hituh <okazysssie@gmail.com>
2022-12-18 22:57:48 +0100

    superturbohiperqury

--- routes/Osoba.js
+++ routes/Osoba.js
@@ -1,6 +1,7 @@
 const db = require("../models");
 const route = require('express').Router()
 var Osoba = db.Osoba
+var Film = db.Film
 
 route.get('/', (req, res) => {
     Osoba.findAll()
@@ -33,23 +34,8 @@ route.post('/findId', (req, res) => {
       });
 }) 
 
-route.post('/', (req, res) => {
-    console.log(req.body)
-    Osoba.create({
-        Imię: req.body.Imię,
-        Nazwisko: req.body.Nazwisko,
-        Data_Urodzenia: req.body.Data_Urodzenia,
-        Kraj_Pochodzenia: req.body.Kraj_Pochodzenia,
-        Zdjęcie: req.body.Zdjęcie,
-    })
-        .then(data => {
-            res.send(data);
-        })
-        .catch(err => {
-            res.status(500).send({
-                message:
-                    err.message || "Some error occurred while creating the Tutorial."
-            });
-        });
-})
+
+
+
+
 exports = module.exports = route
